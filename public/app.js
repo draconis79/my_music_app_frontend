79fac43f7a9e05b9c8ccf01b7f3d87cc0fde63ec
Mandy Tong <mandytong@Mandys-MacBook-Pro.local>
2018-01-12 09:45:40 -0800

    set up angular

--- public/app.js
+++ public/app.js
@@ -1,1 +1,33 @@
 console.log('app.js');
+
+const app = angular.module('album', []);
+
+app.controller('mainController', ['$http', function($http) {
+
+  this.message = 'controller works';
+  this.album = [];
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/albums',
+  }).then(response => {
+    console.log('response: ', response);
+    this.albums = response.data;
+  }).catch(reject => {
+    console.log('reject: ', reject);
+  });
+  this.processForm = () => {
+    console.log('Form data: ', this.formdata);
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/albums',
+      data: this.formdata
+    }).then(response => {
+      console.log('response: ', response);
+      this.albums.unshift(response.data);
+    }).catch(reject => {
+      console.log('reject: ', reject);
+    });
+  };
+
+}]);
