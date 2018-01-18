d8f0405b247e0a17a50d60cc0d6816afa951a2f7
Jaq Dunham <jdunham9@gmail.com>
2018-01-17 23:32:41 -0500

    create route works

--- public/app.js
+++ public/app.js
@@ -5,15 +5,39 @@ const app = angular.module('AlbumReviewApp', []);
 app.controller('mainController', ['$http', function($http) {
 
   this.albums = [];
+  this.album = {};
+  this.showOne = false;
+  this.review = {};
+  this.reviews = [];
 
     $http({
       method: 'GET',
       url: 'http://localhost:3000/albums'
     }).then((response) => {
-      console.log(response.data);
+      console.log("Response:", response.data);
       this.albums = response.data;
     }).catch((err) => {
-      console.log(err);
+      console.log("Error:", err);
     });
+
+    this.albumShow = (album) => {
+      this.showOne = !this.showOne;
+      this.album = album;
+      console.log("Album:", album);
+    };
+
+    this.createReview = () => {
+      // console.log("Submit new review");
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/albums/' + this.album.id + '/reviews',
+        data: this.createForm
+      }).then((response) => {
+        console.log("New review:", response.data);
+        this.review = response.data;
+        this.reviews.unshift(this.review);
+      }).catch((err) => {
+        console.log("Error:", err);
+      })};
 
 }]);
